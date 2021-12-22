e2393a2a0bcfdb227169cc04142020f1d9d12743
Asanga Udara <70792747+asangaudara@users.noreply.github.com>
2021-12-22 18:27:30 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -77,15 +77,10 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("etc/font.otf", 32)
-    draw.text((190, 550), f"Title: {title}", (255, 255, 255), font=font)
-    draw.text((190, 590), f"Duration: {duration}", (255, 255, 255), font=font)
-    draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
-    draw.text(
-        (190, 670),
-        f"Powered By: Asanga Udara",
-        (255, 255, 255),
-        font=font,
-    )
+    draw.text((23, 550), f"Title: {title}", (255, 255, 255), font=font)
+    draw.text((23, 590), f"Duration: {duration}", (255, 255, 255), font=font)
+    draw.text((23, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text((190, 670),f"Powered By: Asanga Udara",(255, 255, 255),font=font,)
     img.save("final.png")
     os.remove("temp.png")
     os.remove("background.png")
